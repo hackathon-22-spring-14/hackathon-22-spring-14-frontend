d18f28d6867a5e164dc69de3167b6ee6ec0d39d1
warabi39 <inuwarabi39@icloud.com>
2022-06-14 22:12:54 +0900

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,30 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/global/Header.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <Header />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+:root {
+  /* 色についての設定 */
+  --headerColor: 
+  /* テキストについての設定 */
+  --fontSizeTitle: 28px;
+  --fontSizeXLarge: 18px;
+  --fontSizeLarge: 16px;
+  --fontSizeMedium: 14px;
+  --fontSizeSmallIcon: 16px;
+  --fontSizeIcon: 26px;
+  --fontSizePart: 48px;
+  --multilineTextLineHeight: 1.5;
+}
+
+html {
+  
 }
 </style>
