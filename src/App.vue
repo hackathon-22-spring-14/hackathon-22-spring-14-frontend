f065bc6af01a4f40d1a14f366a9c74e82b0d7a3e
s9 <63638257+s9tack@users.noreply.github.com>
2022-06-15 19:56:45 +0900

    Revert "Introduced canvas"
    This reverts commit baaa9d93f536b0ef4f7ee589187ec5e34251079c.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import MainPage from './components/MainPage.vue'
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
-  <MainPage />
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <style>
