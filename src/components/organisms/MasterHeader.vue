623234701840431d49f317ad35a7f57fff3fe497
warabi39 <inuwarabi39@icloud.com>
2022-06-18 21:26:55 +0900

    StamQに変更

--- src/components/organisms/MasterHeader.vue
+++ src/components/organisms/MasterHeader.vue
@@ -6,7 +6,7 @@
       <a
         class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
       >
-        <span class="ml-3 text-xl">Stamp</span>
+        <span class="ml-3 text-xl">StamQ</span>
       </a>
       <nav
         class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center"
