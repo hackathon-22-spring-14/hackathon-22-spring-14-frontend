e6551b78187535999fa833375c3ccf19c5e09b69
s9 <63638257+s9tack@users.noreply.github.com>
2022-06-18 22:08:22 +0900

    Revert "login and signup"
    This reverts commit dba8f0dfbb9fb5468a16ab09a4c5a416b753621b.

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Create from './components/pages/CreatePage.vue'
 import Gallery from './components/pages/GalleryPage.vue'
-import Mypage from './components/pages/Mypage.vue'
-import LoginPage from './components/pages/LoginPage.vue'
+import Mypage from './components/pages/MyPage.vue'
 
 const routes = [
   { path: '/', name: 'createPage', component: Create },
@@ -10,8 +9,6 @@ const routes = [
   { path: '/gallery', name: 'galleryPage', component: Gallery },
 
   { path: '/mypage', name: 'myPage', component: Mypage },
-  
-  { path: '/login', name: 'LoginPage', component: LoginPage },
 ]
 
 const router = createRouter({
