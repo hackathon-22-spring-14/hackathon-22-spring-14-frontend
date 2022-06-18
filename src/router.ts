74073a8cdaac9f0432cc769e3d4b25ee33e3c339
s9 <63638257+s9tack@users.noreply.github.com>
2022-06-18 21:43:47 +0900

    loginとsignupのapiを叩けるようにした

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Create from './components/pages/CreatePage.vue'
 import Gallery from './components/pages/GalleryPage.vue'
 import Mypage from './components/pages/MyPage.vue'
+import LoginPage from './components/pages/LoginPage.vue'
 
 const routes = [
   { path: '/', name: 'createPage', component: Create },
@@ -9,6 +10,8 @@ const routes = [
   { path: '/gallery', name: 'galleryPage', component: Gallery },
 
   { path: '/mypage', name: 'myPage', component: Mypage },
+
+  { path: '/login', name: 'LoginPage', component: LoginPage },
 ]
 
 const router = createRouter({
