6c593fca6d2ea83af0fa132074b712d434d39829
s9 <63638257+s9tack@users.noreply.github.com>
2022-06-18 23:13:52 +0900

    whoamiを実装

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import Create from './components/pages/CreatePage.vue'
 import Gallery from './components/pages/GalleryPage.vue'
 import Mypage from './components/pages/MyPage.vue'
 import LoginPage from './components/pages/LoginPage.vue'
+import axios from 'axios'
 
 const routes = [
   { path: '/', name: 'createPage', component: Create },
@@ -11,12 +12,24 @@ const routes = [
 
   { path: '/mypage', name: 'myPage', component: Mypage },
 
-  { path: '/login', name: 'LoginPage', component: LoginPage },
+  { path: '/login', name: 'LoginPage', component: LoginPage, meta: { isPublic: true } },
 ]
 
 const router = createRouter({
   history: createWebHistory(), // HTML5 History モード
   routes,
 })
+
+router.beforeEach(async (to) => {
+  try {
+    await axios.get("/api/whoami");
+  } catch (_) {
+    if (to.meta.isPublic) {
+      return true;
+    }
+    return "/login";
+  }
+  return true;
+});
 
 export default router
